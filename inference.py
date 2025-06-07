17d3133dbb1fb8537f9c2da785cab561fa076737
Derek Zhu <derekzhu2005@gmail.com>
2025-06-07 00:26:55 -0700

    gitignore

--- inference.py
+++ inference.py
@@ -6,6 +6,18 @@ import time
 from model import GPT, GPTConfig
 from tokenizer import tokenize
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+config = GPTConfig(
+    block_size=32,  
+    vocab_size=32,
+    n_layer=1,
+    n_head=4,
+    n_embd=32,
+    dropout=0.0,
+    bias=True
+)
+
 chars = "<s>I love machine learning<e>"
 vocab = sorted(set(tokenize(chars)))
 print(vocab)
@@ -16,66 +28,6 @@ print(itos)
 
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
-
-config = GPTConfig(
-    block_size=32,  
-    vocab_size=len(vocab),
-    n_layer=1,
-    n_head=4,
-    n_embd=32,
-    dropout=0.0,
-    bias=True
-)
-
-data = "<s>I love machine learning<e>"
-tokens = tokenize(data)
-
-if len(tokens) < config.block_size + 1:
-    pad_len = config.block_size + 1 - len(tokens)
-    tokens += ['<e>'] * pad_len
-
-X, Y = [], []
-for i in range(len(tokens) - config.block_size):
-    chunk_input = tokens[i:i + config.block_size]
-    chunk_target = tokens[i + 1:i + config.block_size + 1]
-    X.append(encode(chunk_input))
-    Y.append(encode(chunk_target))
-
-# Convert to torch tensors
-x = torch.tensor(X, dtype=torch.long)
-y = torch.tensor(Y, dtype=torch.long)
-
-print("Input shape:", x.shape)   # (num_examples, block_size)
-print("Target shape:", y.shape)
-
-device = 'cuda' if torch.cuda.is_available() else 'mps'
-model = GPT(config).to(device)
-x, y = x.to(device), y.to(device)
-
-optimizer = model.configure_optimizers(
-    weight_decay=0.0,
-    learning_rate=1e-3,
-    betas=(0.9, 0.95),
-    device_type=device
-)
-
-for step in range(1000):
-    logits = model(x)
-    B, T, C = logits.shape
-    # print("B: ", B)
-    # print("T: ", T)
-    # print("C: ", C)
-
-    logits = logits.view(B * T, C)
-    targets = y.view(B * T)
-    loss = F.cross_entropy(logits, targets)
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    if step % 50 == 0 or step == 499:
-        print(f"Step {step}: Loss = {loss.item():.9f}")
 
 # 6. Generation
 def generate(model, idx, max_new_tokens):
